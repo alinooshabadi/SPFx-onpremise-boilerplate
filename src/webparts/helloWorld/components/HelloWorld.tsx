e2951f2c9eb7a084c4d8aa733262655589ad257c
Ali Nooshabadi <ali.nooshabadi@gmail.com>
2021-02-15 15:59:05 +0330

    Convert webpart to functional component

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -2,24 +2,27 @@ import * as React from 'react';
 import styles from './HelloWorld.module.scss';
 import { IHelloWorldProps } from './IHelloWorldProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { WebPartContext } from '@microsoft/sp-webpart-base';
 
-export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
-  public render(): React.ReactElement<IHelloWorldProps> {
-    return(
+export const webPartContext = React.createContext<WebPartContext>(null);
+
+export const HelloWorld = ({ context, description }: IHelloWorldProps): React.ReactElement => {
+  return (
+    <webPartContext.Provider value={context}>
       <div className={styles.helloWorld}>
         <div className={styles.container}>
           <div className={styles.row}>
             <div className={styles.column}>
-              <span className={styles.title}>Welcome to SharePoint!</span>
+              <span className={styles.title}>{`Welcome to ${context.pageContext.web.title}!`}</span>
               <p className={styles.subTitle}>Customize SharePoint experiences using Web Parts.</p>
-              <p className={styles.description}>{escape(this.props.description)}</p>
+              <p className={styles.description}>{escape(description)}</p>
               <a href='https://aka.ms/spfx' className={styles.button}>
                 <span className={styles.label}>Learn more</span>
               </a>
             </div>
           </div>
         </div>
-      </div >
-    );
-  }
-}
+      </div>
+    </webPartContext.Provider>
+  );
+};
